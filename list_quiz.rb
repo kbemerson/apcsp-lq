009e978fafd61f893e01a5892fbd9390a1471ebd
Katie Emerson <kbemerson27@gmail.com>
2019-10-09 10:48:04 -0600

    Three_Even Done

--- list_quiz.rb
+++ list_quiz.rb
@@ -1,24 +1,19 @@
-# def three_even(list)
-#     count = 0
-#     (list.size).times do |num|
-#         if list[num] % 2 == 0
-#             count += 1
-#             if count == 3
-#                 (list.size).times do |num|
-#                     if (list[num % 2]) == (list[num.size + 1 % 2])
-#                         return "3 Evens"
-#                     end
-#                 end
-#             end
-#         end
-#     end
-#     return "Not 3 Evens"
-# end
-# puts three_even([2, 1, 3, 5])   # Not 3 Evens
-# puts three_even([2, 4, 12, 5])  # 3 Evens
-# puts three_even([2, 1, 4, 6])   # Not 3 Evens
-# puts three_even([1, 4, 6, 4])   # 3 Evens
-# puts three_even([1])            # Not 3 Evens
+def three_even(list)                        # I ended up asking for help on this one. I made it a little too complicated, and ended up making my code shorter. I changed my count from needing three variables to be true to just having one. 
+    count = 0
+    list.each do |num|
+        if num % 2 == 0
+            count += 1
+        end
+    end
+    if count == 3
+        return true
+    else 
+        return false
+    end
+end
+# puts three_even([2, 1, 3, 5])   # False
+# puts three_even([2, 4, 12, 5])  # True
+# puts three_even([1, 6, 4, 1])   # False
 
 
 def bigger_two(list1, list2)                # I realizes I actually didn't need a loop here. I rewrote my code and changed the returns to return just one line. 
@@ -33,31 +28,31 @@ def bigger_two(list1, list2)                # I realizes I actually didn't need 
         return "#{list1}"
     end
 end
-puts bigger_two([1, 2], [3, 4]) # [3, 4]
-puts bigger_two([1, 7], [4, 4]) # [1, 7]
+# puts bigger_two([1, 2], [3, 4]) # [3, 4]
+# puts bigger_two([1, 7], [4, 4]) # [1, 7]
 
 
-# def series_up(n)
-#     n.size.times do |num|
-#         n1 = num.size
-#         n2 = num.size - 1
-#         n3 = num.size - 2
-#         n4 = num.size - 3
-#         if num == 1
-#             return n1
-#         end
-#         if num == 2
-#             return n2, n1, n1
-#         end
-#         if num == 3
-#             return n3, n3, n2, n3, n2, n1, n1
-#         end
-#         if num == 4
-#             return n4, n4, n3, n4, n3, n2, n4, n3, n2, n1
-#         end
-#     end
-# end
-# puts series_up(1)
-# puts series_up(2)
-# puts series_up(3)
+def series_up(n)
+    n.size.times do |num|
+        n1 = n
+        n2 = n - 1
+        n3 = n - 2
+        n4 = n - 3
+        if n == 1
+            return n1
+        end
+        if n == 2
+            return n2, n2, n1
+        end
+        if n == 3
+            return n3, n3, n2, n3, n2, n1
+        end
+        if n == 4
+            return n4, n4, n3, n4, n3, n2, n4, n3, n2, n1
+        end
+    end
+end
+# # puts series_up(1)
+# # puts series_up(2)
+# # puts series_up(3)
 # puts series_up(4)
