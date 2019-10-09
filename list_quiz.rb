094a00733c58b65d439a2e7edfd1a475ea512f16
Katie Emerson <kbemerson27@gmail.com>
2019-10-09 09:56:11 -0600

    Quiz Day 2

--- list_quiz.rb
+++ list_quiz.rb
@@ -1,43 +1,42 @@
-# def three_even(list)
-#     count = 0
-#     list.size.times do |num|
-#         if num % 2 == 0
-#             count = count + 1
-#         end
-#         if count == 3
-#             return 3
-#         else 
-#             return false
-#         end
-#     end
-# end
-# puts three_even([2, 1, 3, 5])   # false
-# puts three_even([2, 4, 12, 5])  # true
-# puts three_even([2, 1, 4, 6])   # false
-# puts three_even([1, 4, 6, 4])   # true
-# puts three_even([1])            # false
+def three_even(list)                # I redid my list.size to make it a little more specific. Before it was only made to return 
+    count = 0                       # if there were 3 evens even if they weren't next to each other. 
+    (list.size).times do |num|
+        if list[num] % 2 == 0
+            count += 1
+            if list[num % 2] == list[(num + 1) % 2]
+                if count == 3
+                    return "3 Evens"
+                end
+            end
+        end
+    end
+    return "Not 3 Evens"
+end
+puts three_even([2, 1, 3, 5])   # Not 3 Evens
+puts three_even([2, 4, 12, 5])  # 3 Evens
+puts three_even([2, 1, 4, 6])   # Not 3 Evens
+puts three_even([1, 4, 6, 4])   # 3 Evens
+puts three_even([1])            # Not 3 Evens
 
 
-# def bigger_two(list1, list2)
-#     list1.size.times do |num|
-#         num1 = num + num
-#         return num1
-#     end
-#     list2.size.times do |num|
-#         num1 = num + num
-#         return num2
-#     end
-#     if num1 > num2
-#         return list1
-#     else num2 > num1
-#         return list2
-#     end
-#     if num1 = num2
-#         return list1
-#     end
-# end
-# puts bigger_two([1, 2], [3, 4]) # [3, 4]
-# puts bigger_two([1, 7], [4, 4]) # [1, 7]
+def bigger_two(list1, list2)
+    list1.size.times do |num|
+        num1 = list1[num] + list1[num + 1]
+    end
+    list2.size.times do |num|
+        num2 = list2[num] + list2[num + 1]
+    end
+    if num1 > num2
+        return list1
+    else num2 > num1
+        return list2
+    end
+    if num1 = num2
+        return list1
+    end
+end
+puts bigger_two([1, 2], [3, 4]) # [3, 4]
+puts bigger_two([1, 7], [4, 4]) # [1, 7]
 
 
 def series_up(n)
